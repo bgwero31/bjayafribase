6cb9a6bc1e23a66c2733ea53d2a72adc63854b39
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-07-04 18:01:29 +0200

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,23 @@
+// src/data/products.js
+const products = [
+  {
+    id: 1,
+    title: "EcoCash T-shirt",
+    description: "Stylish and comfy. Hustler wear.",
+    price: "10 USD",
+    image: "https://i.imgur.com/W4hTndS.png",
+    ownerUID: "user123",
+    ownerName: "Tino"
+  },
+  {
+    id: 2,
+    title: "Solar Powerbank",
+    description: "Charge anywhere, no zesa problems.",
+    price: "25 USD",
+    image: "https://i.imgur.com/7yUvePI.jpeg",
+    ownerUID: "user456",
+    ownerName: "Mai Energy"
+  }
+];
+
+export default products;
