0b3cb063c45b4f8066d21c2edc8832056d8b7ea4
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-07-05 00:57:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
