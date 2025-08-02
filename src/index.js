fa75c7a51e24c58d6f1a2d6ded2a4d709a39916e
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-08-02 05:09:46 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
